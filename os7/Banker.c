61275749801dbcced89656157c55250e4cff24db
sahil161102 <85942526+sahil161102@users.noreply.github.com>
2022-04-06 16:02:12 +0530

    Update Banker.c

--- os7/Banker.c
+++ os7/Banker.c
@@ -4,6 +4,7 @@ struct consumerDemand{
 	int res1;
 	int res2;
 	int res3;
+	int flag = 1;
 };
 
 struct consAvailableResource{
@@ -46,5 +47,26 @@ int main(){
 	scanf("%d",&consAvRes.res2);
 	printf("Enter the available item of resource 3 for consumer %d :\t",i);
 	scanf("%d",&consAvRes.res3);
+	}
+	printf("INVENTORY :\n");
+	printf("resource1\tresource2\tresource3\n");
+	printf("%d\t\t%d\t\t%d\n",inv.res1,inv.res2,inv.res3);
+	printf("AVAILABLE RESOURCES :\n");
+	printf("resource1\tresource2\tresource3\n");
+	for(int i =0;i<n;i++)
+	printf("%d\t\t%d\t\t%d\n",consAvRes[i].res1,consAvRes[i].res2,consAvRes[i].res3);
+  printf("NEEDED RESOURCES :\n");
+	printf("resource1\tresource2\tresource3\n");
+	for(int i =0;i<n;i++)
+		printf("%d\t\t%d\t\t%d\n",consDem[i].res1,consDem[i].res2,consDem[i].res3);
+	while(n--){
+  		for(int i = 0; i<n; i++){
+			if(inv[i].res1>consDem[i].res1 && inv[i].res2>consDem[i].res2 && inv[i].res3>consDem[i].res3 && 			consDem[i].flag){
+				inv.res1 += consDem[i].res1;
+				inv.res2 += consDem[i].res2;
+				inv.res3 += consDem[i].res3;
+				consDem[i].flag = 0;
+			}
+		}
 	}
 }
